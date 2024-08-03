03705ce72407750744c6dd92e367aa75bb5bbe6b
Yaroslav Ilchencko <ilchenckoyaroslav@gmail.com>
2024-08-03 13:47:58 +0300

    refactor: removed the extra function

--- src/index.js
+++ src/index.js
@@ -1,60 +1,57 @@
 module.exports = function toReadable(number) {
-    function cleanString(number) {
-        const readableNumbers = [
-            "",
-            "one",
-            "two",
-            "three",
-            "four",
-            "five",
-            "six",
-            "seven",
-            "eight",
-            "nine",
-            "ten",
-            "eleven",
-            "twelve",
-            "thirteen",
-            "fourteen",
-            "fifteen",
-            "sixteen",
-            "seventeen",
-            "eighteen",
-            "nineteen",
-        ];
-        const tens = [
-            "",
-            "",
-            "twenty",
-            "thirty",
-            "forty",
-            "fifty",
-            "sixty",
-            "seventy",
-            "eighty",
-            "ninety",
-        ];
-        if (number === 0) {
-            return "zero";
+    const readableNumbers = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    if (number === 0) {
+        return "zero";
+    }
+    if (number < 100) {
+        return checkNumber(number);
+    }
+    if (number >= 100 && number < 1000) {
+        return `${readableNumbers[number.toString()[0]]} hundred ${checkNumber(
+            Number(number.toString().slice(1))
+        )}`.trim();
+    }
+    function checkNumber(number) {
+        if (number < 20) {
+            return readableNumbers[number];
         }
-        if (number < 100) {
-            return checkNumber(number);
-        }
-        if (number >= 100 && number < 1000) {
-            return `${
-                readableNumbers[number.toString()[0]]
-            } hundred ${checkNumber(Number(number.toString().slice(1)))}`;
-        }
-        function checkNumber(number) {
-            if (number < 20) {
-                return readableNumbers[number];
-            }
-            if (number >= 20 && number < 100) {
-                return `${tens[number.toString()[0]]} ${
-                    readableNumbers[number % 10]
-                }`;
-            }
+        if (number >= 20 && number < 100) {
+            return `${tens[number.toString()[0]]} ${
+                readableNumbers[number % 10]
+            }`.trim();
         }
     }
-    return cleanString(number).trim();
 };
